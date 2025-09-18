f8197e54dec05811088393c6058ca84ce1f90e89
Shunquan Huang <huangs18@unlv.nevada.edu>
2025-09-18 15:47:53 -0700

    update particle alignment in setup_blast

--- src/setup/setup_blast.cpp
+++ src/setup/setup_blast.cpp
@@ -1,5 +1,6 @@
 #include <iostream>  
-#include <vector>    
+#include <vector>   
+#include <array> 
 #include <stdexcept> 
 #include "setup.h"
 #include "config.h"
@@ -12,9 +13,7 @@ using namespace std;
 // vector<vector<double>> Setup(int nleft, double x0, double xmin, double xmax, double rholeft, double rhoright, double pleft, double pright, double hfact, int nghost){
 void Setup(Particles& pts){
     
-    int nxunify = (int)Config::getInstance().get("nxunify"); 
-    int nyunify = (int)Config::getInstance().get("nyunify"); 
-    int nzunify = (int)Config::getInstance().get("nzunify"); 
+    int N = Config::getInstance().get("N"); 
     double xmin = Config::getInstance().get("xmin");  
     double xmax = Config::getInstance().get("xmax"); 
     double ymin = Config::getInstance().get("ymin");  
@@ -38,43 +37,52 @@ void Setup(Particles& pts){
     double eos_parm = Config::getInstance().get("gamma"); 
     pts.eos = set_eos(0, eos_parm);             // adiabatic
 
-    double dx = 1.0/nxunify;
-    double dy = 1.0/nyunify;
-    double dz = 1.0/nzunify;
-    double dr = cbrt(dx*dy*dz); 
-    int nx = (xmax-xmin)/dx;
-    int ny = (ymax-ymin)/dy;
-    int nz = (zmax-zmin)/dz;
-
-    double m0 = rho*dx*dy*dz; 
+    double V = (xmax - xmin) * (ymax - ymin) * (zmax - zmin);
+    double m0 = rho*V/N; 
+    double a = cbrt(4.0 * V / N);
     // RandomGenerator ranx(-0.05*dx, 0.05*dx); 
     // RandomGenerator rany(-0.05*dy, 0.05*dy); 
     // RandomGenerator ranz(-0.05*dz, 0.05*dz); 
+
+    array<array<double,3>,4> basis = {{
+        {0.0, 0.0, 0.0},
+        {0.5, 0.5, 0.0},
+        {0.5, 0.0, 0.5},
+        {0.0, 0.5, 0.5}
+    }};
     int count = 0; 
-    for (int i = 0; i < nx; i++) {
-        double xnow = xmin + (i + 0.5) * dx;
-        for (int j = 0; j < ny; j++) {
-            double ynow = ymin + (j + 0.5) * dy;
-            for (int k = 0; k < nz; k++) {
-                double znow = zmin + (k + 0.5) * dz;
-                pt.x1 = xnow; 
-                pt.x2 = ynow; 
-                pt.x3 = znow; 
-                pt.den = rho; 
-                pt.mas = m0; 
-                pt.vel1 = 0; 
-                pt.vel2 = 0; 
-                pt.vel3 = 0; 
-                pt.len = hfact*dr;     // 1.2*dxleft
-                pt.ene = elow;
-                if (pt.x1*pt.x1+pt.x2*pt.x2+pt.x3*pt.x3<=r1*r1) {
-                        count++; 
-                    }
-                pts.add_particle(pt); 
+    for (int i = 0; i * a <= (xmax - xmin); i++) {
+        for (int j = 0; j * a <= (ymax - ymin); j++) {
+            for (int k = 0; k * a <= (zmax - zmin); k++) {
+                double shift_x = xmin + i * a;
+                double shift_y = ymin + j * a;
+                double shift_z = zmin + k * a;
+                for (auto &b : basis) {
+                    double xnow = shift_x + b[0] * a;
+                    double ynow = shift_y + b[1] * a;
+                    double znow = shift_z + b[2] * a;
+                    pt.x1 = xnow; 
+                    pt.x2 = ynow; 
+                    pt.x3 = znow; 
+                    pt.den = rho; 
+                    pt.mas = m0; 
+                    pt.vel1 = 0; 
+                    pt.vel2 = 0; 
+                    pt.vel3 = 0; 
+                    pt.len = hfact*a;     // 1.2*dxleft
+                    pt.ene = elow;
+                    if (pt.x1*pt.x1+pt.x2*pt.x2+pt.x3*pt.x3<=r1*r1) {
+                            count++; 
+                        }
+                    pts.add_particle(pt); 
+                }
             }
         }
     }
 
+    if (count == 0) {
+        throw runtime_error("No particles found inside r1, check your setup!");
+    }
     double ehigh = totale/count; 
     for (int i=0; i<pts.endid; i++){
         if (pts.x1[i]*pts.x1[i] + pts.x2[i]*pts.x2[i] + pts.x3[i]*pts.x3[i]<=r1*r1){
